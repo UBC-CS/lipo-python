bbb5efff9c28b327867008f61bababf14e67fd08
bradleypick <bradleypick@gmail.com>
2018-05-12 20:09:59 -0700

    working adaptive_lipo implementation

--- src/sequential.py
+++ src/sequential.py
@@ -3,6 +3,7 @@ Sequential algorithms for minimizing expensive functions
 """
 
 import numpy as np
+from scipy.spatial.distance import pdist, squareform
 
 def lipo(func, bounds, k, n):
     """
@@ -84,7 +85,7 @@ def prs(func, bounds, n):
     }
     return output
 
-def adaptive_lipo(func, bounds, n, k_seq, p):
+def adaptive_lipo(func, bounds, n, k_seq=np.logspace(-10,10,10000), p=0.5):
     """
     Parameters
     ----------
@@ -111,28 +112,39 @@ def adaptive_lipo(func, bounds, n, k_seq, p):
     
     x.append(x_prop)
     y.append(func(x[0]))
+    k = 0
 
     lower_bound = lambda x_prop, y, x, k: np.max(y-k*np.linalg.norm(x_prop-x))
 
-    for t in np.arange(t):
-        b = np.random.binomial(n=1, p=p)
-        if b == 1:
-            u = np.random.uniform(size=len(bounds))
-            x_prop = u * (bound_maxs - bound_mins) + bound_mins
-        else:
-            # randomly sample from the set of potential minimizers
-            # element x contained within bounds such that there exists 
-            # a lipschitz continuous function g that equals our y = f(x_t) 
-            # values for all t and the element x minimizes the function g
-            # how (if at all) can this be linked to the lower bound?
+    for t in np.arange(n):
+
+        # draw a uniformly distributed random variable
+        u = np.random.uniform(size=len(bounds))
+        x_prop = u * (bound_maxs - bound_mins) + bound_mins
+
+        # check if we are exploring or exploiting
+        if not np.random.binomial(n=1, p=p):
+            # exploiting - must ensure we're drawing from potential minimizers
+            while lower_bound(x_prop, y, x, k) > np.min(y):
+                u = np.random.uniform(size=len(bounds))
+                x_prop = u * (bound_maxs - bound_mins) + bound_mins
         
+        # once settled on proposal add it to the seen points
         x.append(x_prop)
         y.append(func(x_prop))
         best.append(np.min(y))
 
-        # update estimate of the lipschitz constant
-        # should be the minimum value in k_seq 
-        # that is compatible with our observed x, y
+        # update estimate of lipschitx constant
+        # compute pairwise differences between y values
+        y_outer = np.outer(np.ones(len(y)), y)
+        y_diff = np.abs(y_outer - y_outer.T)
+        # compute distance matrix between x values
+        x_dist = squareform(pdist(np.array(x)))
+        np.fill_diagonal(x_dist, np.Inf)
+        # estimate lipschitz constant
+        k_est = np.max(y_diff / x_dist)
+        k = k_seq[np.argmax(k_seq > k_est)]
+
 
     output = {
         'loss': np.array(best).reshape(n),
