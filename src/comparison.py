6f892ccd339eea039ff7a95c05a098c07935ad85
bradleypick <bradleypick@gmail.com>
2018-05-13 18:54:12 -0700

    small changes to reflect sequential update

--- src/comparison.py
+++ src/comparison.py
@@ -7,7 +7,7 @@
 #
 # Usage: python comparison.py filename='imagename.png' [--num_sim, --num_iter]
 
-from sequential import lipo, prs, adaptive_lipo
+from sequential import lipo, pure_random_search, adaptive_lipo
 from plotting import loss_v_iter
 
 import numpy as np
@@ -28,15 +28,10 @@ if args.function == "holder_table":
 
     def f(x):
         inside_exp = np.abs(1-np.sqrt(x[0]*x[0]+x[1]*x[1])/np.pi)
-        return -np.abs(np.sin(x[0])*np.cos(x[1])*np.exp(inside_exp))
+        return np.abs(np.sin(x[0])*np.cos(x[1])*np.exp(inside_exp))
 
     #k = 40
     bnds = [(-10,10),(-10,10)]
-
-# an even simpler 1-d example
-#def g(x):
-#    return (np.cos(x) + 2*np.cos(np.pi*x) - np.sin(np.pi/2*x))
-#bnds = [(0,4*np.pi)]
 
 #----------------------------------------------------#
 # THE SEQUENTIAL STRATEGIES SHOULD ALSO BE ARGUMENTS #
@@ -50,7 +45,7 @@ def main():
     for sim in np.arange(args.num_sim):
 
         lipo_output = adaptive_lipo(func=f, bounds=bnds, n=args.num_iter)
-        prs_output = prs(func=f, bounds=bnds, n=args.num_iter)
+        prs_output = pure_random_search(func=f, bounds=bnds, n=args.num_iter)
 
         results_lipo[sim,:] = lipo_output['loss']
         results_prs[sim,:] = prs_output['loss']
