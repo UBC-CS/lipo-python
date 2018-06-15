de1e9ba38573d5064e3736b1d4c9a7daab12ea4f
Bradley Pick <bradleypick@gmail.com>
2018-06-14 21:59:33 -0700

    clean this

--- run.sh
+++ run.sh
@@ -14,13 +14,3 @@ python src/format_data.py data/raw/yacht_hydrodynamics.data data/clean/ data/yac
 
 # this does not work - formatting of auto-mpg is super irritating
 #python src/format_data.py data/raw/auto-mpg.data data/clean/ '\t' data/auto-names.txt --response=mpg --drop='car-name'
-
-
-
-#python src/synthetic-comparison.py --filename=data/simulation_results --num_sim=20 --num_iter=100
-
-#python src/fig-5-generator.py --inputfile=data/simulation_results.pickle --outputfile=aggregate_results90 --target=0.9
-
-#python src/fig-5-generator.py --inputfile=data/simulation_results.pickle --outputfile=aggregate_results95 --target=0.95
-
-#python src/fig-5-generator.py --inputfile=data/simulation_results.pickle --outputfile=aggregate_results99 --target=0.99
